07f841f35e51a13a673e0807d71eb5676a96bf45
sohagmia360 <gdsohag360@gmail.com>
2022-04-17 13:50:18 +0600

    hero section make responsive

--- src/Routing/Routing.js
+++ src/Routing/Routing.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 
 
+
 const Routing = () => {
     return (
         <Routes>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import Hero from '../components/Hero/Hero';
+import Services from '../components/Services/Services';
 
 const Home = () => {
     return (
         <div className=' mx-auto'>
             <Header />
             <Hero />
+            <Services />
         </div>
     );
 };
